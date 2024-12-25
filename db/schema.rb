df94fe930499b705c07b61501c6d341bd59a78c2
ksk-n <k.021759@gmail.com>
2024-12-26 01:23:45 +0900

    migration を実行

--- db/schema.rb
+++ db/schema.rb
@@ -16,21 +16,31 @@ ActiveRecord::Schema.define(version: 2024_12_24_155034) do
   enable_extension "plpgsql"
 
   create_table "article_likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_article_likes_on_article_id"
+    t.index ["user_id"], name: "index_article_likes_on_user_id"
   end
 
   create_table "articles", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "article_id", null: false
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +67,8 @@ ActiveRecord::Schema.define(version: 2024_12_24_155034) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "article_likes", "articles"
+  add_foreign_key "article_likes", "users"
+  add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
 end
