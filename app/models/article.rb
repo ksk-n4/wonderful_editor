d11d06f9d60816425341031bbeb991052ff00bed
ksk-n <k.021759@gmail.com>
2025-01-05 18:55:55 +0900

    各モデルにバリデーションを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -17,4 +17,6 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
+
+  validates :title, presence: true
 end
