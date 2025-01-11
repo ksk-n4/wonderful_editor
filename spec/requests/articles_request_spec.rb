cabe76dd2cbcbee4adc9cce4e4af658e26d39478
ksk-n <k.021759@gmail.com>
2025-01-11 12:10:30 +0900

    rubocop に適応するようにコードを修正

--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe "Articles", type: :request do
   describe "GET /articles" do
     subject { get(api_v1_articles_path) }
 
-    let!(:article1) { create(:article, updated_at: 3.days.ago) }
-    let!(:article2) { create(:article, updated_at: 1.days.ago) }
-    let!(:article3) { create(:article) }
+    before { create_list(:article, 3) }
 
     it "記事の一覧が取得できる" do
       subject
