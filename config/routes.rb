4573f7fbb084701c0b657877a08701a799da57aa
ksk-n <k.021759@gmail.com>
2025-02-28 20:07:51 +0900

    マイページの記事一覧を取得する API を実装

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,11 @@ Rails.application.routes.draw do
       namespace :articles do
         resources :drafts, only: [:index, :show]
       end
+
+      namespace :current do
+        resources :articles, only: [:index]
+      end
+
       resources :articles
     end
   end
